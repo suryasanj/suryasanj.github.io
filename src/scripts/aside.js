1cd272acc31bbd49d7706967fccba841b2ae5f54
Surya Sanjay <105813716+suryasanj@users.noreply.github.com>
2026-04-02 11:21:29 -0400

    updates

--- src/scripts/aside.js
+++ src/scripts/aside.js
@@ -1,5 +1,5 @@
 export async function loadAside() {
-  const res = await fetch('./src/snippets/aside.html');
+  const res = await fetch('https://suryasanj.github.io/src/snippets/aside.html');
   if (!res.ok) throw new Error(`HTTP ${res.status}`);
   document.getElementById('aside-placeholder').innerHTML = await res.text();
 }
